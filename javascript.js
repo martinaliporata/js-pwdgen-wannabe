50502d9b1eb03d96dc2094d3475dd8b9b39d9139
martinaliporata <martinaliporata@gmail.com>
2024-05-07 17:53:54 +0200

    added three const variables

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina:<nomecognomecolorepreferito24>
+
+const userName = prompt('Write your name');
+
+const userSurname = prompt('Write your surname');
+
+const userFavouriteColor = prompt('Write your favourite color');
+
+console.log(userName + userSurname + userFavouriteColor + '24')
